57b309ec1fba5a743a16ae295d365a1bfec6b32f
Kimberly Lockwood <nvj5@cdc.gov>
2023-12-12 09:36:15 -0500

    Adding code and files for Day 2 solution

---
+++ Day 2/day2solution.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(stringi)
+
+data <- read_document("day2.txt") %>% as.data.frame()
+
+table <- data %>%
+  separate_wider_delim(data, cols = ., delim = ":", names = c("id", "match")) %>%
+  mutate(game = stri_extract_first_regex(id, "[0-9]+")) %>%
+  separate_longer_delim(match, delim = ";") %>%
+  group_by(game) %>%
+  mutate(option = 1:n()) %>%
+  separate_longer_delim(match, delim = ",") %>%
+  mutate(count = stri_extract_first_regex(match, "[0-9]+")) %>%
+  mutate(color = str_extract_all(match, "[[a-z]]+")) %>%
+  ungroup()
+
+table_check <- table %>%
+  mutate(count = as.numeric(count)) %>%
+  group_by(game, option) %>%
+  mutate(check = ifelse(color == "red" & count > 12, "fail",  
+                    ifelse(color == "green" & count > 13, "fail", 
+                        ifelse(color == "blue" & count > 14, "fail", "pass")))) %>%
+  mutate(failn = ifelse(check == "fail", "1", "0")) %>%
+  mutate(failn = as.numeric(failn)) %>%
+  group_by(game) %>%
+  mutate(failsum = sum(failn)) %>%
+  filter(failsum == 0) %>%
+  mutate(game = as.numeric(game))
+
+table_unique <- unique(table_check$game)
+
+answer  <- sum(table_unique)
+  
+answer
